0b8546f442601a18d61408601c83f5a981195e62
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-02-15 11:28:41 -0300

    Utilizando Middlewares

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        res.status(400).json({error: "Customer not found"});
+    }
+
+    req.customer = customer;
+
+    next();
+}
+
 app.post("/account", (req, res) => {
     const {cpf, name} = req.body;
 
@@ -27,15 +41,8 @@ app.post("/account", (req, res) => {
     res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-    const { cpf } = req.headers;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if(!customer) {
-        res.status(400).json({error: "Customer not found"});
-    }
-
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
     res.json(customer.statement);
 })
 
