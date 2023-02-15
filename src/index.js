fdd59b998a7cede1ef61e2de2617b8f1a803b27b
Douglas Martins <douglas.martins@auditornfe.com.br>
2023-02-15 09:51:17 -0300

    Utilizando os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json([{mensagem: "Hello World!"}]);
+app.get("/courses", (req, res) => {
+    res.json(["Curso 1", "Curso 2", "Curso 3"]);
+})
+
+app.post("/courses", (req, res) => {
+    res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.put("/courses/:id", (req, res) => {
+    res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.patch("/courses/:id", (req, res) => {
+    res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+})
+
+app.delete("/courses/:id", (req, res) => {
+    res.json(["Curso 6", "Curso 7", "Curso 4"]);
 })
 
 app.listen(3333, () => {
